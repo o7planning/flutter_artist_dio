4b1c79435aa09f13d7ca0e06679ac87cd7809a59
o7planning <10128477+o7planning@users.noreply.github.com>
2025-06-15 17:13:39 +0600

    .

--- lib/flutter_artist_dio.dart
+++ lib/flutter_artist_dio.dart
@@ -11,6 +11,11 @@ part 'src/v1/_error_handler/__handle_dio_exception.dart';
 part 'src/v1/_error_handler/__handle_dio_response.dart';
 part 'src/v1/_error_handler/__handle_exception.dart';
 part 'src/v1/_model/request_log_info.dart';
+//
+part 'src/v1/api/_delete.dart';
+part 'src/v1/api/_get.dart';
+part 'src/v1/api/_post.dart';
+part 'src/v1/api/_put.dart';
 part 'src/v1/core/__base.dart';
 part 'src/v1/core/_read_token_from_headers.dart';
 part 'src/v1/core/_write_token_to_headers.dart';
@@ -20,6 +25,7 @@ part 'src/v1/json/_json_delete.dart';
 part 'src/v1/json/_json_get.dart';
 part 'src/v1/json/_json_post.dart';
 part 'src/v1/json/_json_put.dart';
+//
 part 'src/v1/logger/rest_logger.dart';
 
 class FlutterArtistDio {
@@ -45,6 +51,100 @@ class FlutterArtistDio {
       FaDioInterceptor(
         readTokenFromHeaders: readTokenFromHeaders,
       ),
+    );
+  }
+
+  Future<ApiResult<D>> restGet<D>(
+    String path, {
+    ResponseDataMode responseDataMode = ResponseDataMode.realData,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? queryParameters,
+    String? token,
+    required Converter<D>? converter,
+    ErrorConverter errorConverter = defaultErrorConverter,
+    bool showDebug = false,
+  }) async {
+    return _jsonGet<D>(
+      dio,
+      path,
+      responseDataMode: responseDataMode,
+      headers: headers,
+      queryParameters: queryParameters,
+      token: token,
+      converter: converter,
+      errorConverter: errorConverter,
+      showDebug: showDebug,
+    );
+  }
+
+  Future<ApiResult<D>> restPost<D>(
+    String path, {
+    ResponseDataMode responseDataMode = ResponseDataMode.realData,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? queryParameters,
+    dynamic data,
+    required Converter<D>? converter,
+    ErrorConverter errorConverter = defaultErrorConverter,
+    bool showDebug = false,
+    Duration? receiveTimeout,
+  }) async {
+    return await _jsonPost<D>(
+      dio,
+      path,
+      responseDataMode: responseDataMode,
+      headers: headers,
+      queryParameters: queryParameters,
+      data: data,
+      converter: converter,
+      errorConverter: errorConverter,
+      showDebug: showDebug,
+      receiveTimeout: receiveTimeout,
+    );
+  }
+
+  Future<ApiResult<D>> restPut<D>(
+    String path, {
+    ResponseDataMode responseDataMode = ResponseDataMode.realData,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? queryParameters,
+    dynamic data,
+    required Converter<D>? converter,
+    ErrorConverter errorConverter = defaultErrorConverter,
+    bool showDebug = false,
+  }) async {
+    return _jsonPut<D>(
+      dio,
+      path,
+      responseDataMode: responseDataMode,
+      headers: headers,
+      queryParameters: queryParameters,
+      data: data,
+      converter: converter,
+      errorConverter: errorConverter,
+      showDebug: showDebug,
+    );
+  }
+
+  Future<ApiResult<D>> restDelete<D>(
+    String path, {
+    ResponseDataMode responseDataMode = ResponseDataMode.realData,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? queryParameters,
+    dynamic data,
+    required Converter<D>? converter,
+    ErrorConverter errorConverter = defaultErrorConverter,
+    bool showDebug = false,
+  }) async {
+    return _jsonDelete<D>(
+      dio,
+      path,
+      responseDataMode: responseDataMode,
+      headers: headers,
+      queryParameters: queryParameters,
+      data: data,
+      converter: converter,
+      errorConverter: errorConverter,
+      showDebug: showDebug,
     );
   }
 }
---
+++ lib/src/v1/api/_put.dart
@@ -0,0 +1,60 @@
+part of '../../../flutter_artist_dio.dart';
+
+// Future<Response<T>> put<T>(
+//     String path, {
+//       Object? data,
+//       Map<String, dynamic>? queryParameters,
+//       Options? options,
+//       CancelToken? cancelToken,
+//       ProgressCallback? onSendProgress,
+//       ProgressCallback? onReceiveProgress,
+//     });
+Future<ApiResult<D>> _put<D>(
+  Dio dio,
+  String path, {
+  ResponseDataMode responseDataMode = ResponseDataMode.realData,
+  Map<String, dynamic>? headers,
+  Map<String, dynamic>? queryParameters,
+  dynamic data,
+  required Converter<D>? converter,
+  ErrorConverter errorConverter = defaultErrorConverter,
+  bool showDebug = false,
+}) async {
+  int restRequestId = 0;
+  try {
+    headers ??= {};
+    restRequestId = _addRequestIdToHeaders(headers: headers);
+    //
+    final response = await dio.put(
+      path,
+      options: Options(
+        headers: headers,
+        receiveTimeout: const Duration(seconds: 10),
+      ),
+      queryParameters: queryParameters,
+      data: data,
+    );
+    //
+    return _handleDioResponse<D>(
+      responseDataMode: responseDataMode,
+      response: response,
+      converter: converter,
+      restRequestId: restRequestId,
+      showDebug: showDebug,
+    );
+  } on DioException catch (e, stackTrace) {
+    return _handleDioException(
+      e,
+      stackTrace: stackTrace,
+      restRequestId: restRequestId,
+      showDebug: showDebug,
+    );
+  } catch (e, stackTrace) {
+    return _handleException(
+      e,
+      stackTrace: stackTrace,
+      restRequestId: restRequestId,
+      showDebug: showDebug,
+    );
+  }
+}
