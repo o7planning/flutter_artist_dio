8ff462e952952b5fc24609ab691556f282cee38a
o7planning <10128477+o7planning@users.noreply.github.com>
2025-06-15 17:54:20 +0600

    .

--- lib/rest_debug_screen.dart
+++ lib/rest_debug_screen.dart
@@ -4,19 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_artist_commons_ui/flutter_artist_commons_ui.dart';
 
-import 'src/v0/_utils_/json_utils.dart';
-import 'src/v0/flutter_artist_dio.dart';
+import 'flutter_artist_dio.dart';
+import 'src/v1/_utils_/json_utils.dart';
 
-part 'src/v0/_dialog/_rest_debug_dialog.dart';
-part 'src/v0/debug/_utils.dart';
-part 'src/v0/debug/debug_section.dart';
-part 'src/v0/debug/section/_dio_path_section.dart';
-part 'src/v0/debug/section/_dio_request_info_section.dart';
-part 'src/v0/debug/section/_dio_request_list_section.dart';
-part 'src/v0/debug/section/_dio_response_section.dart';
-part 'src/v0/debug/widget/_custom_app_container.dart';
-part 'src/v0/debug/widget/_data_view.dart';
-part 'src/v0/debug/widget/_map_key_value_view.dart';
+part 'src/v1/_dialog/_rest_debug_dialog.dart';
+part 'src/v1/debug/_utils.dart';
+part 'src/v1/debug/debug_section.dart';
+part 'src/v1/debug/section/_dio_path_section.dart';
+part 'src/v1/debug/section/_dio_request_info_section.dart';
+part 'src/v1/debug/section/_dio_request_list_section.dart';
+part 'src/v1/debug/section/_dio_response_section.dart';
+part 'src/v1/debug/widget/_custom_app_container.dart';
+part 'src/v1/debug/widget/_data_view.dart';
+part 'src/v1/debug/widget/_map_key_value_view.dart';
 
 class RestDebugScreen extends StatelessWidget {
   static const String routeName = "/restDebugScreen";
